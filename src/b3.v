1930c88e8fef6c2ff53987649d67a058a28362d8
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2022-05-25 17:09:45 +0300

    dz3

--- src/b3.v
+++ src/b3.v
@@ -10,6 +10,42 @@ Import ListNotations.
 Section NatDict'Proofs.
   Context {V: Type}.
 
+ Lemma n_eq_or_neq (n n': nat): (n=n')\/(n<>n').
+  Proof.
+  pattern n, n'.
+  apply (nat_double_ind).
+  {
+    intros n0.
+    destruct n0.
+    auto.
+    auto.
+  }
+  {
+    intros n0.
+    auto.
+  }
+  {
+    intros n0 m.
+    intros h.
+    destruct h.
+    auto.
+    auto.
+  }
+Qed.
+
+
+Lemma noeq_impl_nat_eq_false (n1:nat) (n2:nat) : n1 <> n2 -> nat_eq n1 n2 = false.
+    Proof.
+      intros.
+      remember H as H'.
+      remember (nat_eq_neg_spec n1 n2) as nat_eq_neg.
+      unfold iff in nat_eq_neg.
+      destruct nat_eq_neg as (nat_eq_neg_l & nat_eq_neg_r).
+      (*Reset Heqn1.*)
+      auto.
+    Qed.
+    
+
   (* Prove that 'remove' operation actually removes a key. *)
   (* The list inside nat_dict_list consists of pairs.
      To extract components of such list's head, 
@@ -18,13 +54,61 @@ Section NatDict'Proofs.
   Lemma removed_not_contained (d: @nat_dict_list V) (n: nat):
     contains'' (remove'' d n) n = false.
   Proof.
+  induction d.
+  auto.
+
+(*  unfold contains''.
+  unfold get''.*)
+  destruct a.
+  destruct (n_eq_or_neq n n0).
+  {
+    apply eq_implies_nat_eq in H.
+    simpl.
+    rewrite H.
+    auto.
+  }
+  apply noeq_impl_nat_eq_false in H.
+  simpl.
+  rewrite H.
+  simpl.
+  rewrite H.
+  
+(*rewrite Heqaaa.
+  pattern (remove'' d n), n in Heqaaa.
+   
+  pattern (remove'' d n), n.
+  fold (@contains'' V).
+  fold ((contains'' (remove'' d n) n)).
+  inversion (get'' (remove'' d n) n).
+  
+  fold (@contains'' V).
+  
+  unfold contains'' in IHd.
+  
+  fold (@contains'' V) n (remove'' d n).
+  IHd.
+  unfold contains'' in IHd.
+  red in IHd.
+  rewrite IHd.
+  destruct IHd.
+  auto.
+*)
+  
+
+  
+  
+  
 Admitted.
 
   (* Define a mapping function similar to one defined for regular lists.
      It should replace values stored in dict but keep them under the same keys. *)
   Fixpoint map'' {W: Type} (f: V -> W) (d: @nat_dict_list V): @nat_dict_list W :=
       (* place your code here *)
-  .
+      match d with
+      | nil => nil
+      | cons (k, v) t => cons (k, f v) (map'' f t)
+      end.
+
       
   (* Prove that a value stored in a mapped dict 
      requires a corresponding value stored in an original dict. *)
@@ -33,14 +117,91 @@ Admitted.
       (get'' (map'' m d) n = Some w) <->
       (exists v, get'' d n = Some v /\ m v = w).
   Proof.
-Admitted.
+  unfold iff.
+  split.
+  {
+    intros.
+    induction d as [| a l ih].
+    {unfold get''.
+      simpl in H.
+      inversion H.
+    }
+    destruct a.
+    destruct (n_eq_or_neq n n0).
+    {
+      rewrite H0.
+      simpl.
+      exists v.
+      (*eapply ex_intro.*)
+      rewrite nat_eq_refl.
+      split.
+      {reflexivity. }
+      rewrite H0 in H.
+      simpl in H.
+      rewrite nat_eq_refl in H.
+      inversion H.
+      reflexivity.
+    }
+    simpl in H.
+    remember H0 as H0'.
+    remember (nat_eq_neg_spec n n0) as nat_eq_neg.
+    unfold iff in nat_eq_neg.
+    destruct nat_eq_neg as (nat_eq_neg_l & nat_eq_neg_r).
+    (*Reset Heqn1.*)
+    remember (nat_eq_neg_r H0') as H1.
+    rewrite H1 in H.
+    apply ih in H.
+
+    unfold get''.
+    simpl.
+    rewrite H1.
+    auto.
+    }
+
+    intros.
+    destruct H as (v & H).
+    destruct H as (H1 & H2).
+    induction d as [| a l ih].
+    {
+      simpl in H1.
+      inversion H1.
+    }
+    destruct a.
+    destruct (n_eq_or_neq n n0).
+    {
+    apply eq_implies_nat_eq in H.
+    simpl in H1.
+    rewrite H in H1.
+    simpl.
+    rewrite H.
+    inversion H1.
+    destruct H2.
+    reflexivity.
+    }
+    apply noeq_impl_nat_eq_false in H.
+    simpl in H1.
+    rewrite H in H1.
+    simpl.
+    rewrite H.
+    auto.
+Qed.
+    
+    
+
+
+
 
   (* Implement a filtering function. 
      The result should contain only those keys whose values satisfy the predicate;
      in this case they remain unchanged. *)
   Fixpoint filter'' {U: Type} (f: U -> bool) (d: @nat_dict_list U): @nat_dict_list U :=
       (* place your code here *)
-  .
+      match d with
+      | nil => nil
+      | cons (k, v) t => if (f v) then cons (k, v) (filter'' f t) else (filter'' f t)
+      end.
+
+  
 
 (*...*)
 
@@ -50,6 +211,40 @@ Admitted.
       (contains'' (filter'' f d) n = true) <->
       (exists v, get'' d n = Some v /\ f v = true).
   Proof.
+  intros.
+  unfold iff.
+  split.
+  {
+    induction d.
+    {
+      intros.
+      simpl in H.
+      inversion H.
+    }
+    destruct a.
+    destruct (n_eq_or_neq n n0).
+    {
+       apply (eq_implies_nat_eq n n0) in H.
+       intros.
+       exists v.
+       unfold get''.
+       simpl.
+       rewrite H.
+       split.
+       reflexivity.
+       unfold contains'' in H0.
+       simpl in H0.
+       remember (bool_true_or_false (f v)).
+       destruct o.
+       apply e.
+       rewrite e in H0.
+       apply IHd in H0.
+       desrtuct 
+       red in  H0.
+       
+
+
+  
 Admitted.
 
 
